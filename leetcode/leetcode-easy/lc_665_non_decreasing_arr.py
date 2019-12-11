fe394b25dd769118ee7c2cf9deae76a0e7c03adf
neil4droid <cterse@gmail.com>
2019-12-12 00:22:44 +0530

    665 Non-decreasing array solved

--- leetcode/leetcode-easy/lc_665_non_decreasing_arr.py
+++ leetcode/leetcode-easy/lc_665_non_decreasing_arr.py
@@ -2,13 +2,23 @@ from typing import List
 
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        mySet = set()
-        for i in range(1, len(nums)):
-            num_slice = nums[slice(i)]
-            if nums[i] < max(num_slice):
-                mySet.add(i)
-            if len(mySet) > 1:
-                return False
-        if len(mySet) <= 1:
+        nums_len = len(nums)
+        if nums_len == 0 or nums_len == 1:
             return True
-            
+        else:
+            count = 0
+            for i in range(0, nums_len-1):
+                if nums[i+1] < nums[i]:
+                    if count > 1:
+                        return False
+                    count += 1
+                    if i == nums_len-2 or i == 0:
+                        continue
+                    elif nums[i+2] >= nums[i]:
+                        i += 2
+                    elif nums[i+2] < nums[i]:
+                        if nums[i+1] < nums[i-1]:
+                            count += 1
+            if count <= 1:
+                return True
+            return False
