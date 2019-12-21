8c2a04b19f45989f87c78df6e7e27ebb44af2462
neil4droid <cterse@gmail.com>
2019-12-22 01:30:04 +0530

    204: Complete counting primes using the sieve

--- leetcode/leetcode-easy/lc_204_count_primes.py
+++ leetcode/leetcode-easy/lc_204_count_primes.py
@@ -1,53 +1,56 @@
 import math
 from typing import List
 import time
+import random
 
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n == 0 or n == 1 or n == 2:
+        if n <= 2:
             return 0
-        count = 0
+        count = 1
         for i in range(3, n):
             if self.is_prime(i):
                 count += 1
         return count
 
-    def countPrimesUsingSieve(self, n: int) -> int:
-        if n == 0 or n == 1 or n == 2:
-            return 0
-        primes_list = list()
-        for i in range(0, n):
-            primes_list.append(1)
-        primes_list[0] = primes_list[1] = 0
-        primes_list[2] = 1
-        for i in range(3, n):
-            if i < n and self.is_prime(i):
-                primes_list[i] = 1
-                primes_list = self.cross_out_multiples(i, n, primes_list)
-        
-        return primes_list.count(1)
-    
-    def cross_out_multiples(self, prime_no: int, n: int, primes_list:List[int]) -> List[int]:
-        i = prime_no
-        i *= i
-        while i <= n:
-            primes_list[i] = 0
-            i *= i
-        return primes_list
-
     def is_prime(self, n: int) -> bool:
         if n <= 1:
             return False
+        if n == 2:
+            return True
         sqrt_n_plus_1 = math.ceil(math.sqrt(n))+1
         for i in range(2, sqrt_n_plus_1):
             if n % i == 0:
                 return False
         return True
+    
+    def countPrimesUsingSieve(self, n:int) -> int:
+        if n <= 2:
+            return 0
+        primes_list = list()
+        for _ in range(0, n):
+            primes_list.append(1)
+        primes_list[0] = primes_list[1] = 0
+        for i in range(2, math.ceil(n/2)):
+            if not self.is_prime(i):
+                primes_list[i] = 0
+            primes_list = self.__run_sieve_over_list(i, n, primes_list)
+        return primes_list.count(1)
+    
+    def __run_sieve_over_list(self, number:int, n:int, input_list:List[int]) -> List[int]:
+        i = number + number
+        while i < n:
+            input_list[i] = 0
+            i += number
+        return input_list
 
 sol = Solution()
-start = time.process_time()
-print(sol.countPrimes(499979))
-print("Time elapsed: ", time.process_time() - start, 's')
-start = time.process_time()
-print(sol.countPrimes(999983))
-print("Time elapsed: ", time.process_time() - start)
+for _ in range(0, 10):
+    random_int = random.randint(0, 200000000)
+    # print("Naive:", random_int, ": ")
+    # start = time.process_time()
+    # print(sol.countPrimes(random_int), '   ', time.process_time() - start,'s')
+    print("Sieve:", random_int, ": ")
+    start = time.process_time()
+    print(sol.countPrimesUsingSieve(random_int), '  ',  time.process_time() - start,'s\n')
+    # print("Time elapsed: ", time.process_time() - start, 's')
