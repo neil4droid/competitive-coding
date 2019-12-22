1350fb8c95bb2ed2a6355eb340712cf61f394874
neil4droid <cterse@gmail.com>
2019-12-23 01:12:27 +0530

    204: Some time calculations

--- leetcode/leetcode-easy/lc_204_count_primes.py
+++ leetcode/leetcode-easy/lc_204_count_primes.py
@@ -28,30 +28,35 @@ class Solution:
         if n <= 2: return 0
         primes_list = [1] * n
         primes_list[0] = primes_list[1] = 0
-        fn_is_prime = self.is_prime
         fn_run_sieve = self.__run_sieve_over_list
         # for i in range(2, math.ceil(n/2)):  # This was the original logic I thought of
-        for i in range(2, math.ceil(math.sqrt(n))):
-            if not fn_is_prime(i):
-                primes_list[i] = 0
+        for i in range(2, math.ceil(math.sqrt(n))):       # original, 0.201640625s average time for n upto 1500000
+        # i = 2
+        # while i*i < n:      # 0.210546875s average time for n upto 1500000
             if primes_list[i] == 1:
                 primes_list = fn_run_sieve(i, n, primes_list)
+            # i += 1
         return primes_list.count(1)
     
     def __run_sieve_over_list(self, number:int, n:int, input_list:List[int]) -> List[int]:
-        i = number + number
+        i = number*2
         while i < n:
             input_list[i] = 0
             i += number
         return input_list
 
 sol = Solution()
-for _ in range(0, 10):
-    random_int = random.randint(0, 1000000)
-    print("Naive:", random_int, ": ")
-    start = time.process_time()
-    print(sol.countPrimes(random_int), '   ', time.process_time() - start,'s')
+total_time_elapsed = float()
+test_cases_count = 200
+for _ in range(0, test_cases_count):
+    random_int = random.randint(0, 1500000)
+    # print("Naive:", random_int, ": ")
+    # start = time.process_time()
+    # print(sol.countPrimes(random_int), '   ', time.process_time() - start,'s')
     print("Sieve:", random_int, ": ")
     start = time.process_time()
-    print(sol.countPrimesUsingSieve(random_int), '  ',  time.process_time() - start,'s\n')
+    print(sol.countPrimesUsingSieve(random_int), '\n')
+    time_elapsed = time.process_time() - start
+    total_time_elapsed += time_elapsed
     # print("Time elapsed: ", time.process_time() - start, 's')
+print('Total time elapsed: ', total_time_elapsed/test_cases_count)
