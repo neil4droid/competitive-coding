3bd07e28f65a59ab0676dd9d1173f2a17d15ff3b
neil4droid <cterse@gmail.com>
2019-12-25 16:13:49 +0530

    121: Faster method for buying and selling stocks.
    Couldn't think of it even though it was so easy. Maybe because it was since I knew I had to use DP and was thinking along those lines.

--- leetcode/leetcode-easy/lc_121_stocks.py
+++ leetcode/leetcode-easy/lc_121_stocks.py
@@ -1,18 +1,30 @@
 import random
 from typing import List
 
+
 class Solution:
     def maxProfitNaive(self, prices: List[int]) -> int:
-        max_profit = [None]*3
-        for i in range(0, len(prices)-1):
-            for j in range(i+1, len(prices)):
+        max_profit = [None] * 3
+        for i in range(0, len(prices) - 1):
+            for j in range(i + 1, len(prices)):
                 if max_profit[0] is None or prices[j] - prices[i] > max_profit[0]:
                     max_profit = [prices[j] - prices[i], i, j]
         return max_profit if max_profit[0] is not None and max_profit[0] >= 0 else 0
 
+    def max_profit(self, prices: List[int]) -> int:
+        if len(prices) == 0: return 0
+        min = prices[0]
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] - min > max_profit:
+                max_profit = prices[i] - min
+            if prices[i] < min:
+                min = prices[i]
+        return max_profit
 
-print('[7,1,5,3,6,4] : ', Solution().maxProfitNaive([7,1,5,3,6,4]))
-print('[7,6,4,3,1] : ', Solution().maxProfitNaive([7,6,4,3,1]))
+
+print("[7,1,5,3,6,4] : ", Solution().max_profit([7, 1, 5, 3, 6, 4]))
+print("[7,6,4,3,1] : ", Solution().max_profit([7, 6, 4, 3, 1]))
 
 input_count = random.randint(1, 10)
 for i in range(0, input_count):
@@ -20,4 +32,4 @@ for i in range(0, input_count):
     input_list = list()
     for _ in range(0, input_length):
         input_list.append(random.randint(0, 10))
-    print(input_list, ': ', Solution().maxProfitNaive(input_list))
+    print(input_list, ": ", Solution().max_profit(input_list))
