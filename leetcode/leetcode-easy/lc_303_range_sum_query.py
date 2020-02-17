3bc5f0bb2bb4d6699cd8871d4cb485fed60d8db2
neil4droid <cterse@gmail.com>
2020-02-17 21:06:54 +0530

    303: Improve code

--- leetcode/leetcode-easy/lc_303_range_sum_query.py
+++ leetcode/leetcode-easy/lc_303_range_sum_query.py
@@ -1,21 +1,23 @@
 from typing import List
 
 class NumArray:
-    
-    dp = None
-    
     def __init__(self, nums: List[int]):
-        if len(nums) == 0: pass
-        else:
-            self.dp = [0]*len(nums)
-            self.dp[0] = nums[0]
-            for i in range(1, len(nums)):
-                self.dp[i] = nums[i] + self.dp[i-1]
-            
+        self.nums_arr = nums[:]
+        self.memo = self.__create_memo(nums)
+        
+    def __create_memo(self, nums: List[int]) -> List[int]:
+        if not nums: return []
+
+        ret_arr = []
+        ret_arr.append(nums[0])
+        for i in range(1, len(nums)):
+            ret_arr.append(nums[i] + ret_arr[i-1])
+        return ret_arr
+
     def sumRange(self, i: int, j: int) -> int:
-        if self.dp is None: return None
-        if i == 0:  return self.dp[j]
-        return self.dp[j] - self.dp[i-1]
+        if not self.memo: return 0
+        if i == 0: return self.memo[j]
+        return self.memo[j] - self.memo[i-1]
 
 
 # Your NumArray object will be instantiated and called as such:
