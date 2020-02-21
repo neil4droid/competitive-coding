4c9d14a7ae061cfb7ed554f64405db2b86a16ecb
neil4droid <cterse@gmail.com>
2020-02-22 02:15:57 +0530

    1347: Solve using collections.Counter

--- leetcode/leetcode-medium/lc_1347_make_two_strings_anagram.py
+++ leetcode/leetcode-medium/lc_1347_make_two_strings_anagram.py
@@ -1,9 +1,21 @@
 import unittest
+from collections import Counter
 
 class Solution(unittest.TestCase):
     def minSteps(self, s: str, t: str) -> int:
         # return self.minSteps_using_dict(s, t)
         return self.minSteps_naive(s, t)
+
+    def minSteps_using_counter(self, s: str, t: str) -> int:
+        """
+        collection.Counter = dict subclass having the index->index-count mappings.
+        Adding a counter to an empty counter removes the non-positive indexes.
+
+        Runtime: 112 ms, faster than 76.63% of Python3 online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+        
+        Memory Usage: 13.3 MB, less than 100.00% of Python3 online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+        """
+        return sum(((Counter(s) - Counter(t)) + Counter()).values())
 
     def minSteps_naive(self, s: str, t: str) -> int:
         """
@@ -23,21 +35,23 @@ class Solution(unittest.TestCase):
 
         return count
 
-
     def minSteps_using_dict(self, s: str, t: str) -> int:
         """ 
         Create a dict of letter->letter-count in s string.
         For every letter in t, subtract letter count from dict.
         Return total sum of all letter counts.
         
-        Runtime: 372 ms, faster than 10.49% of Python3 online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+        Runtime: 356 ms, faster than 10.49% of Python3 online submissions for Minimum Number of Steps to Make Two Strings Anagram.
         
         Memory Usage: 13.2 MB, less than 100.00% of Python3 online submissions for Minimum Number of Steps to Make Two Strings Anagram.
         """
         s_dict = {}
 
         for i in range(0, len(s)):
-            s_dict[s[i]] = s_dict[s[i]] + 1 if s_dict.get(s[i]) else 1
+            s_dict[s[i]] = 0
+        
+        for i in range(0, len(s)):
+            s_dict[s[i]] += 1
         
         for i in range(0, len(t)):
             if s_dict.get(t[i]): s_dict[t[i]] -= 1
@@ -55,6 +69,12 @@ class Solution(unittest.TestCase):
         self.assertEquals(self.minSteps_naive("leetcode", "practice"), 5)
         self.assertEquals(self.minSteps_naive("anagram", "mangaar"), 0)
         self.assertEquals(self.minSteps_naive("friend", "family"), 4)
+    
+    def test_minSteps_using_counter(self):
+        self.assertEquals(self.minSteps_using_counter("bab", "bbb"), 1)
+        self.assertEquals(self.minSteps_using_counter("leetcode", "practice"), 5)
+        self.assertEquals(self.minSteps_using_counter("anagram", "mangaar"), 0)
+        self.assertEquals(self.minSteps_using_counter("friend", "family"), 4)
 
 if __name__ == "__main__":
     unittest.main()
