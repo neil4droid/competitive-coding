fe907f4c7f1ad00ac928a8993f97e0d71f6ca30a
neil4droid <cterse@gmail.com>
2020-02-24 01:03:59 +0530

    1277: Add DP solution, copied from the web

--- leetcode/leetcode-medium/lc_1277_count_square_submatrices.py
+++ leetcode/leetcode-medium/lc_1277_count_square_submatrices.py
@@ -3,7 +3,62 @@ from typing import List
 
 class Solution(unittest.TestCase):
     def countSquares(self, matrix: List[List[int]]) -> int:
-        return self.countSquares_naive(matrix)
+        # return self.countSquares_naive(matrix)
+        # return self.count_squares_dp(matrix)
+        return self.count_squares_dp_2(matrix)
+
+    def count_squares_dp(self, matrix: List[List[int]]) -> int:
+        """
+        Wrong answer
+        """
+        if not matrix: return []
+
+        r, c = len(matrix), len(matrix[0])
+        dp = []
+
+        for i in range(0, r):
+            rcount = 0
+            temp_list = []
+            for j in range(0, c):
+                if matrix[i][j] == 1: rcount += 1
+                else: rcount = 0
+                temp_list.append(rcount)
+            dp.append(temp_list)
+
+        count = 0
+        max_length = min(r, c)
+        for i in range(0, r):
+            for j in range(0, c):
+                for length in range(1, max_length+1):
+                    if i+length-1 >= r: 
+                        break
+                    if dp[i][j] >= length and dp[i+length-1][j] >= length: 
+                        count += 1
+                    else: 
+                        break
+        
+        return count
+
+    def count_squares_dp_2(self, matrix: List[List[int]]) -> int:
+        """
+        Method shown in: https://youtu.be/_Lf1looyJMU
+
+        Runtime: 672 ms, faster than 70.82% of Python3 online submissions for Count Square Submatrices with All Ones.
+        
+        Memory Usage: 14.7 MB, less than 100.00% of Python3 online submissions for Count Square Submatrices with All Ones.
+        """
+        if not matrix: return 0
+        
+        r, c = len(matrix), len(matrix[0])
+        dp = [[0]*(c+1) for _ in range(0, r+1)]
+
+        count = 0
+        for i in range(1, r+1):
+            for j in range(1, c+1):
+                dp[i][j] = matrix[i-1][j-1] if matrix[i-1][j-1] == 0 else min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])+1
+                count += dp[i][j]
+        
+        return count
 
     def countSquares_naive(self, matrix: List[List[int]]) -> int:
         """
