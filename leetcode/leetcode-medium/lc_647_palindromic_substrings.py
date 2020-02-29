0fd2b49563802b8ebcabb5ae4b7e8830dc6624f9
neil4droid <cterse@gmail.com>
2020-03-01 00:15:03 +0530

    647: Write DP solution to Palindromic Substrings count

--- leetcode/leetcode-medium/lc_647_palindromic_substrings.py
+++ leetcode/leetcode-medium/lc_647_palindromic_substrings.py
@@ -2,8 +2,32 @@ import unittest
 
 class Solution(unittest.TestCase):
     def countSubstrings(self, s: str) -> int:
-        return self.count_palindromic_substrings_naive(s)
+        """
+        https://leetcode.com/problems/palindromic-substrings/
+        """
+        # return self.count_palindromic_substrings_naive(s)
+        return self.count_palindromic_substrings_dp(s)
     
+    def count_palindromic_substrings_dp(self, s: str) -> int:
+        if not s: return 0
+
+        dp, count = [[0] * len(s) for _ in range(0, len(s))], 0
+        for j in range(0, len(s)):
+            for i in range(0, len(s)):
+                if i+j >= len(s): continue
+                if j == 0:
+                    count += 1
+                    dp[i][j] = 1
+                elif s[i] == s[i+j]:
+                    if j == 1: 
+                        dp[i][j] = 1
+                        count += 1
+                    else:
+                        if dp[i+1][j-2]:
+                            dp[i][j] = 1
+                            count += 1
+        return count
+        
     def count_palindromic_substrings_naive(self, s: str) -> int:
         """
         For every character in the string, check is every substring starting with that character is a palindrome.
@@ -42,6 +66,10 @@ class Solution(unittest.TestCase):
         return s[0] == s[-1] and self.is_palindrome_recursive(s[1:len(s)-1])
 
     def test_countSubstrings(self):
+        ip, op = "a", 1
+        self.assertEqual(self.countSubstrings(ip), op)
+        ip, op = "ab", 2
+        self.assertEqual(self.countSubstrings(ip), op)
         ip, op = "abc", 3
         self.assertEqual(self.countSubstrings(ip), op)
         ip, op = "aaa", 6
